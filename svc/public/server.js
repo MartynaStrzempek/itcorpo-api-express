f8018d08ec1754d4d97c4ee286694d8c81cf5275
Modrzejewski <marek.modrzejewski@atos.net>
2020-04-15 14:01:18 +0200

    refactor and add cvs file read

--- svc/public/server.js
+++ svc/public/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 
-import { getProjectWithEmployees } from './api'
-// const { getProjectWithEmployees } = require('./api')
+import API from './api/index'
 
 // CHAIN OF RESPONSIBILITY // pierwszy który obsłuży - zamyka temat
 // middleware // każdy może coś dodać, ale tylko jeden może wysłać odpowiedź
@@ -18,13 +17,14 @@ app.get('/', (req, res, next) => {
 app.get('/projects/:id', async (req, res, next) => {
   const projectId = req.params.id
   console.log(`received request with projectId:${projectId}`)
-  const project = await getProjectWithEmployees(projectId)
+  const project = await API.getProjectWithEmployees(projectId)
   res.status(200).send(project)
 
   next()
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Listening on PORT:${PORT}`)
-  console.log(`test w`)
+  const test = await API.getBenefitsFromFile();
+  //console.log(test);
 })
